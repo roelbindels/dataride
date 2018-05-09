ba3a84473299de2131938fcdc1eb97703465ff7a
Roel Bindels <roel.bindels@inetprocess.com>
2018-05-09 14:38:57 +0200

    added global functions

--- dataride/__init__.py
+++ dataride/__init__.py
@@ -3,5 +3,5 @@
 """Top-level package for dataride."""
 
 __author__ = """Roel Bindels"""
-__email__ = 'roel.bindels@inetprocess.com'
+__email__ = 'roel.bindels@bitech-development.nl'
 __version__ = '0.1.0'
